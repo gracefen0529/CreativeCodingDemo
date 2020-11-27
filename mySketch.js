a109b93cf9c09413c1493bbb5b4806234df8b844
gracefen0529 <39553697+gracefen0529@users.noreply.github.com>
2020-11-27 16:06:56 +0800

    Add files via upload

--- mySketch.js
+++ mySketch.js
@@ -1,40 +1,85 @@
+let capture
+let cacheGraphics // 儲存物件中每一張影像
+let noise2
+
+function preload(){
+	noise2 = loadImage("noise2.jpeg")
+}
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(640,480);
 	background(100);
+	capture = createCapture(VIDEO)
+	capture.size(640,480)
+	cacheGraphics = createGraphics(640,480)
+	cacheGraphics.translate(640,0)
+	cacheGraphics.scale(-1,1)
+	capture.hide()  // 藏起原生的物件
 }
 
-// -----為了重複擴大動作，在此設定黑、白眼睛-----
-// -----用push、pop包覆語法，可避免內外設定值互相影響-----
-function eye(x,y,sc) {    // eye設定x,y,scale，方便後續直接指定數值
-	
-	push()
-		translate(x,y)	// 定義translate位置為x,y，方便後續填入數值
-		scale(sc)			 // 定義scale代號為sc，方便後續填入數值
-	
-		// 眼球白底
-		fill(255)
-		ellipse(0,0,200) 
-		// 眼珠黑底
-		fill(0)
-		let ang = atan2(mouseY-y,  // 取出x差與y差的角度
-									mouseX-x) 
-		rotate(ang)  // 告知p5在繪製方形之前先跟隨角度 
-		ellipse(50,0,100)	
-	pop()
-	
-}
-
-
-// -----繪出眼睛-----
+var mode = 2  // 預設模式為1
 function draw() {
+	cacheGraphics.image(capture,0,0) // 製作一個暫存的圖片檔案
+	// image(capture,mouseX,mouseY)  // 影像像畫筆一樣匯出
+	noStroke()
+	scale(1)
 	background(0)
 	
-	// o指定往height(Y軸)方向重複 
-	for (var o=0;o<height;o+=100) {
-		
-		// i指定width(X軸)方向重複 
-		for (var i=0;i<width;i+=100) { 			
-			eye(i,o,0.4) // 將x,y,scale帶入指定位置，記得將i與o帶入以利控制
-		}
+	// 避免mouseX取值小於0當機,設定為mouseX跟0,mouseX小於0時直接取0
+	let span = 12+ max(mouseX,0)/20
+	for(var i=0;i<cacheGraphics.width;i+=span){     // x軸重複
+			for(var o=0;o<cacheGraphics.width;o+=span){   // y軸重複
+				let pixel = cacheGraphics.get(i,o)  // 指定使用i,o這位置的顏色做填充
+				
+				// 用pixel[0]+pixel[1]+pixel[2]分別代表RGB,算出平均亮度值
+				let bk = (pixel[0]+pixel[1]+pixel[2])/3  
+				fill(255)
+				if (mode==1){
+					// 原本bk是0~255,重新定位黑到白0~1,黑的地方乘0沒有圈圈,白的地方乘1維持不變
+					ellipse(i,o,span*map(bk,0,255,0,1)) 
+				}
+				if (mode==2){
+					fill(pixel)
+					push()
+						colorMode(HSB)
+						fill(pixel[0],100,90)
+						translate(i,o)
+						rotate(pixel[0]/100) // 每隔pixel各自旋轉
+						rectMode(CENTER) // 方形以中心點旋轉
+						rect(mouseX/10,mouseY/10,span*0.3 + pixel[2]/10) 
+					
+						// 在每個pixel方形中間加小圓型裝飾
+						fill(0)
+						ellipse(0,0,5)
+					pop()
+				}
+				if (mode==3){
+					let txt = "一二三四五田電龍龕鑫"
+					// 使用清單需加上int取整數,深色(10)對應鑫,淺色(0)對應一二三字
+					let bkid = int(map(bk,0,255,10,0)) 
+					
+					fill(pixel[0]+50,pixel[1]+50,pixel[2]+50)
+					textSize(span)
+					textStyle(BOLD)
+					text(txt[bkid],i,o)  // 取txt裡的第[bkid]個字
+				}
+			}
+		} 
+	push()
+		blendMode(MULTIPLY)
+		image(noise2,0,0,640,480)
+	pop()
+}
+
+// 切換模式
+function keyPressed(){
+	if (key=="1"){
+		mode = 1
+	}
+	if (key=="2"){
+		mode = 2
+	}
+	if (key=="3"){
+		mode = 3
 	}
 }
